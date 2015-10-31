070b3508581101e31a2e9e1061eec40478b7cb5a
Jon Shier <jon@jonshier.com>
2015-10-31 17:25:28 -0400

    Initial commit.

---
+++ SwiftyNotificationCenter/SwiftyNotificationCenter.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyNotificationCenter.h
+//  SwiftyNotificationCenter
+//
+//  Created by Jon Shier on 10/31/15.
+//  Copyright © 2015 Jon Shier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyNotificationCenter.
+FOUNDATION_EXPORT double SwiftyNotificationCenterVersionNumber;
+
+//! Project version string for SwiftyNotificationCenter.
+FOUNDATION_EXPORT const unsigned char SwiftyNotificationCenterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyNotificationCenter/PublicHeader.h>
+
+
